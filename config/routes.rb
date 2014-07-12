e483b4c58aaad8b5c958a6359d94d63b5ba20fd1
ariasja <jason.a.arias@vanderbilt.edu>
2014-07-11 17:39:48 -0700

    updated like model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope module: :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :events, only: [:show]
+      resources :posts, only: [:show]
     end
   end
 end
