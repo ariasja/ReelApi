284fefbf01a9d851d8ea50a81bd619c4faca8464
ariasja <jason.a.arias@vanderbilt.edu>
2014-07-28 19:55:03 -0700

    Setting it up for heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load application ENV vars and merge with existing ENV vars. Loaded here so can use values in initializers.
+ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+
 module ReelApi
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
--- db/migrate/20140712012154_remove_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemovePosts < ActiveRecord::Migration
-  def change
-    drop_table :posts
-  end
-end
